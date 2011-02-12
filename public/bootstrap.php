1bf94b6df948ef6bfd5e55aafd2d10f6a6663313
Julian Møller <julian@julianmoeller.dk>
2011-02-12 22:27:49 +0100

    Set up Slim with some options

---
+++ public/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ . "/../lib/Slim/slim/Slim.php";
+require __DIR__ . "/../lib/idiorm/idiorm.php";
+require __DIR__ . "/../lib/paris/paris.php";
+require __DIR__ . "/../lib/TView.php";
+
+Slim::init(array(
+    "view" => "TView"
+  , "templates_dir" => __DIR__ . "/../pages"
+  , "twig.directory" => __DIR__ . "/../lib/Twig/lib/Twig"
+  , "twig.options" => array("auto_reload" => true, "strict_variables" => true, "debug" => true)
+));
+
+Slim::run();
